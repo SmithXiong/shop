fd39b0fc5b41ec23b38d99a28152678d4b8c678a
SmithXiong <xm864802180@163.com>
2017-11-07 17:53:57 +0800

    add order

--- pages/order/order.js
+++ pages/order/order.js
@@ -1,18 +1,44 @@
 // pages/order/order.js
+import { ajax, plus, multi } from '../../utils/util.js';
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    address: {},
+    isHasAddr: false,
+    cart: [],
+    total: 0
+  },
+
+  //计算总价
+  calcuPrice: function (cart) {
+    let total = 0;
+    cart.forEach((o) => {
+      total = plus(total, multi(o.price, o.count))
+    })
+    return total;
+  },
+
+  goAddress: function () {
+    app.navTo('address');
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    try {
+      let cart = wx.getStorageSync('cart') || [];
+      let addrlist = wx.getStorageSync('addrlist') || [];
+      let isHasAddr = addrlist.length === 0 ? false : true;
+      let total = this.calcuPrice(cart);
+      this.setData({ cart: cart, total: total, address: addrlist[0], isHasAddr: isHasAddr })
+    } catch (e) {
+      console.log(e)
+    }
   },
 
   /**
@@ -26,7 +52,14 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    let addrlist = wx.getStorageSync('addrlist') || [];
+    let isHasAddr = addrlist.length === 0 ? false : true;
+    if (this.data.address == undefined) {
+      this.setData({
+        address: addrlist[0],
+        isHasAddr: isHasAddr
+      })
+    }
   },
 
   /**
